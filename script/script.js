98078b67650bc3ea5d55f23fcaa2b2ff56ce31da
LilMus <mustapha.seghir@gmail.com>
2022-05-24 04:24:00 +0200

    fist version

---
+++ script/script.js
@@ -0,0 +1,6 @@
+const questions = document.getElementsByTagName("h2");
+const arrows = document.getElementsByClassName("arrows");
+
+questions[0].addEventListener("click", function () {
+	console.log("cliqué");
+});
